bae279736c3be18002e0eae9eefae31a8c94abc2
Jacek Piętal <prozi85@gmail.com>
2016-06-14 22:45:45 +0200

    has stripe

---
+++ README.md
@@ -0,0 +1,19 @@
+# Install
+
+```bash
+npm install
+```
+
+## Be sure to have those in PROJECT_ROOT
+
+```bash
+stripe.order.json
+firebase.creditentials.json
+stripe.api.key.json
+```
+
+# Run
+
+```bash
+npm start
+```
--- private/stripe/index.js
+++ private/stripe/index.js
@@ -2,6 +2,38 @@
 
 module.exports = (app) => {
 
-	console.log('almost has stripe');
+  const fs   = require('fs');
+  const path = require('path');
+
+  const stripeApiKeyPath = path.join(__dirname, '..', '..', 'stripe.api.key.json');
+
+  fs.exists(stripeApiKeyPath, (exists) => {
+    if (exists) {
+      const stripeApiKey = require(stripeApiKeyPath).apiKey;
+      const stripe = require('stripe')(stripeApiKey);
+      onSuccess(stripe);
+    } else {
+      console.log('you need to put PROJECT_ROOT/stripe.api.key.json with {apiKey:"your api key"}');
+    }
+  });
+
+  function onSuccess(stripe) {
+
+    console.log('has stripe');
+
+    stripe.customers.create({
+      email: 'foo-customer@example.com'
+    }).then(function(customer) {
+      return stripe.charges.create({
+        amount: 1600,
+        currency: 'usd',
+        customer: customer.id
+      });
+    }).then(function(charge) {
+      // New charge created on a new customer
+    }).catch(function(err) {
+      // Deal with an error
+    });
+  }
 
 };
